70cc7f689ddb81258469a2e9f48d6af14895ea40
Dmitriy <dmitry.lupol@gmail.com>
2022-12-02 16:02:43 +0200

    form

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.forms import forms
+from .models import Product
 
-# Register your models here.
+class MovieAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
