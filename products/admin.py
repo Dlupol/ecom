0284b827842e57e5b39c63d87f25fe20c4042cf1
Dmitriy <dmitry.lupol@gmail.com>
2022-12-02 16:17:23 +0200

    formsadmin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django import forms
-from .models import Product
+from .models import Product, Category, Review, Rating, RatingStar
 
 
 class ProductAdminForm(forms.ModelForm):
@@ -8,3 +8,40 @@ class ProductAdminForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'url')
+    list_display_links = ('title',)
+
+
+class ReviewInLine(admin.ModelAdmin):
+
+    model = Review
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = '__all__'
+    list_filter = ('category', 'year')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'parent')
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+
+    list_display = ('star', 'movie', 'ip')
+
+
+admin.site.register(RatingStar)
