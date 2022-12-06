4dc94026bc612c109be231f0aba46d697d97ee95
Dmitriy <dmitry.lupol@gmail.com>
2022-12-06 11:36:48 +0200

    cart

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
+    path('register/', RegisterView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
