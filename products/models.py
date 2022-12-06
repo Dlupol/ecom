4a6d58ac3d16bb282293c16b9995b8018fe80c69
Dmitriy <dmitry.lupol@gmail.com>
2022-12-06 23:47:31 +0200

    cart

--- products/models.py
+++ products/models.py
@@ -15,15 +15,15 @@ class Artist(models.Model):
 
 class Category(models.Model):
 
-    title = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=100)
     url = models.SlugField(max_length=160, unique=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.category_title)
+        self.slug = slugify(self.category_name)
         super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.category_title
+        return self.category_name
 
 
 class Product(models.Model):
