48b6f3fa3a968f304f9f4b4b059e59c3dcff70bc
Dmitriy <dmitry.lupol@gmail.com>
2022-12-06 11:42:35 +0200

    cart

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 
